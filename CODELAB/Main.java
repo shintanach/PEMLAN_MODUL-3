355fed83d4f271cf70ecd245a379cf02380e84ff
shintanach <shintalyaa06@gmail.com>
2025-10-22 12:39:50 +0700

    first commit

--- CODELAB/Main.java
+++ CODELAB/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println();
         Hewan hewan2 = new Hewan("Anjing", "Mamalia","Woof-Woof!!");
         hewan2.tampilkaninfo();
+        Hewan hewan = new Hewan("Kambing", "Mamalia", "Mbek-mbek");
+        hewan.tampilkaninfo();
     }
 
 }
